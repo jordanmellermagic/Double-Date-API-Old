c135edccc7dbe88e19dccb3abca729b13070913f
jordanmellermagic <jordanmellermagic@gmail.com>
2025-12-08 22:43:29 -0800

    Update server.js

--- server.js
+++ server.js
@@ -117,4 +117,6 @@ app.get("/status", (req, res) => {
 
 // ----------------------------------------
 
-app.listen(3000, () => console.log("API running on port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`API running on port ${PORT}`));
+
